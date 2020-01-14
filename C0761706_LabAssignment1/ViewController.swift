90976abc8122c1f2cd82264f9b1ea061b637a3d2
Ramanpreet Singh <ramanpreet230@gmail.com>
2020-01-14 15:57:10 -0500

    Initial Commit

---
+++ C0761706_LabAssignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0761706_LabAssignment1
+//
+//  Created by Ramanpreet Singh on 2020-01-14.
+//  Copyright © 2020 Ramanpreet Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
