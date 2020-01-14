79d7e5a1a9648b6b5898b9292b65605aba41653e
Ramanpreet Singh <ramanpreet230@gmail.com>
2020-01-14 17:45:49 -0500

    Added map.

--- C0761706_LabAssignment1/ViewController.swift
+++ C0761706_LabAssignment1/ViewController.swift
@@ -7,14 +7,98 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    var userLocation: CLLocationCoordinate2D?
+    var destination: CLLocationCoordinate2D?
+    
+    
+    private var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initViews()
     }
-
-
+    
+    private func initViews() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        // Check for Location Services
+        if (CLLocationManager.locationServicesEnabled()) {
+            locationManager.requestAlwaysAuthorization()
+            locationManager.requestWhenInUseAuthorization()
+        }
+        //Zoom to user location
+        if let userLocation = locationManager.location?.coordinate {
+            self.userLocation = userLocation
+        }
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapPress))
+        tapGesture.numberOfTapsRequired = 2
+        mapView.addGestureRecognizer(tapGesture)
+        locationManager.startUpdatingLocation()
+    }
+    
+    @objc func tapPress(gestureRecognizer: UIGestureRecognizer) {
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.removeOverlays(mapView.overlays)
+        destination = nil
+        let touchPoint = gestureRecognizer.location(in: mapView)
+        let coordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        let annotation = MKPointAnnotation()
+        annotation.title = "Destination"
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
+        self.destination = coordinate
+    }
+    
+    @IBAction func getRouteButtonClicked(_ sender: Any) {
+        getRoute()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation: CLLocation = locations[0]
+        let lat = userLocation.coordinate.latitude
+        let long = userLocation.coordinate.longitude
+        let latDelta: CLLocationDegrees = 0.5
+        let longDelta: CLLocationDegrees = 0.5
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        let location = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        self.userLocation = location
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func getRoute() {
+        guard let currentLocation = userLocation, let destination = destination else {
+            return
+        }
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: MKPlacemark(coordinate: currentLocation, addressDictionary: nil))
+        request.destination = MKMapItem(placemark: MKPlacemark(coordinate: destination, addressDictionary: nil))
+        request.requestsAlternateRoutes = true
+        request.transportType = .automobile
+        let directions = MKDirections(request: request)
+        directions.calculate { [unowned self] response, error in
+            guard let unwrappedResponse = response else {
+                print(error?.localizedDescription ?? "")
+                return
+            }
+            let route = unwrappedResponse.routes[0]
+            self.mapView.addOverlay(route.polyline)
+            self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+        }
+    }
+    
 }
 
+extension UIViewController: MKMapViewDelegate {
+    
+    public func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(polyline: overlay as! MKPolyline)
+        renderer.strokeColor = UIColor.blue
+        return renderer
+    }
+}
